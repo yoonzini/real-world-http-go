4af26fc875eabd4c6a5f6d7ff3ae8eff0c840b0e
Yoonjin <elodie@kakao.com>
2020-10-26 18:12:54 +0900

    http 클라이언트 요청 - GET 테스트 코드

--- ch1/server.go
+++ ch1/server.go
@@ -14,7 +14,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Println(string(dump))
-	fmt.Fprintf(w, "<html><body>Hello</bod></html>\n")
+	fmt.Fprintf(w, "<html><body>Hello<input name='attachment-file' type='file'></bod></html>\n")
 }
 
 func main() {
