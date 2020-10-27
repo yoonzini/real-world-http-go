e1ffe698be1718c7b1a8ef8593baaa63bddf4cdf
Yoonjin <elodie@kakao.com>
2020-10-27 18:14:15 +0900

    GET 메서드+쿼리 전송

--- simpleget/main.go
+++ simpleget/main.go
@@ -4,10 +4,15 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 func main() {
-	resp, err := http.Get("http://localhost:18888")
+	values := url.Values{ //쿼리 문자열 선언
+		"query": {"Hello world"},
+	}
+	//url 끝에 문자열로 만든 결과를 추가한다.
+	resp, err := http.Get("http://localhost:18888" + "?" + values.Encode()) //문자열로 만든다.
 	if err != nil {
 		panic(err)
 	}
